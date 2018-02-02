488e69bb7c7b83e2bc7ebdc4fe88de3a0039af01
Luis Rojo <desantiagolab@hotmail.com>
2018-02-02 12:42:08 -0700

    probando firebase

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			while (true)
+			{
+				DateTime date = DateTime.Now;
+
+				string Date = date.ToString("yyyy:MM:dd");
+				string Time = date.ToString("HH:mm:ss");
+
+				var json = JsonConvert.SerializeObject(new
+				{
+					Name = Time,
+					Value = Date,
+				});
+				var request = WebRequest.CreateHttp("https://hola-mundo-c1fd2.firebaseio.com/.json");
+				request.Method = "POST";
+				request.ContentType = "application/json";
+				var buffer = Encoding.UTF8.GetBytes(json);
+				request.ContentLength = buffer.Length;
+				request.GetRequestStream().Write(buffer, 0, buffer.Length);
+				var repose = request.GetResponse();
+				json = (new StreamReader(repose.GetResponseStream())).ReadToEnd();
+			}
+		}
+	}
+}
